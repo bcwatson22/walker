9619a2c8d93ac8fe204831b9bc898e2491c42b4d
Billy Watson <watson@Snowmen.local>
2018-10-28 16:36:19 +0000

    copy change on Services

--- src/components/_common/Services.vue
+++ src/components/_common/Services.vue
@@ -4,7 +4,7 @@
       <article class="text">
         <h1>Services</h1>
         <p>The spectrum of building projects across the Peak District and throughout Derbyshire demanding high-quality plastering is boundless; Bakewell Plastering has contributed to many of these.</p>
-        <p>From listed buildings, barn conversions, shops and holiday lets; to the creation of a new home or simply enhancing an existing one - modern or traditional - the team has the experience and expertise to make it happen!</p>
+        <p>From listed buildings, barn conversions, shops and holiday lets; to the creation of a new home or simply enhancing an existing one - modern or traditional - the team has the experience and expertise!</p>
         <p>Our plastering and damp proofing services extend to rendering, drylining and lime plastering.</p>
         <p>One of the business' priorities is to help achieve your desired interior. Whether it's just smooth, straight lines or dramatic curves and uneven, coarse wall surfaces, Bakewell Plastering will help to realise the anticipated finish.</p>
       </article>
